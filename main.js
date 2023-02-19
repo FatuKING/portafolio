729527a818168cc158c37832d2b68902d18e9567
Facundo Cozzani <cozzanifacundo@gmail.com>
2023-02-18 23:38:19 -0300

    primer commit

---
+++ main.js
@@ -0,0 +1,7 @@
+// Estilos css
+import './style.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap-icons/font/bootstrap-icons.css'
+
+// JS
+import 'bootstrap/dist/js/bootstrap.min.js'
